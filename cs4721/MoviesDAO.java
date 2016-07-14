790cbef6d589362b5e62142ff7ce9f7e1e180317
justin <justin@europa>
2016-07-14 14:19:15 -0400

    Added getTopMovies by genre

--- cs4721/MoviesDAO.java
+++ cs4721/MoviesDAO.java
@@ -18,11 +18,43 @@ public class MoviesDAO {
     private Connection con;
     private Statement stmt;
     
-    public ArrayList<Movie> getTopMovies(int k){
+    public ArrayList<Movie> getTopMovies(int limit){
         String query = "SELECT DISTINCT id, title, year, rtAudienceScore, "
                 + "rtPictureURL, imdbPictureURL FROM movies "
                 + "ORDER BY rtAudienceScore DESC "
-                + "LIMIT " + k;
+                + "LIMIT " + limit;
+        ResultSet rs = null;
+        ArrayList<Movie> movieList = new ArrayList<Movie>();
+        try{
+            con = ConnectionFactory.getConnection();
+            stmt = con.createStatement();
+            rs = stmt.executeQuery(query);
+            while(rs.next()){
+                int id = rs.getInt("id");
+                String title = rs.getString("title");
+                int year = rs.getInt("year");
+                int rtAudienceScore = rs.getInt("rtAudienceScore");
+                String rtPictureURL = rs.getString("rtPictureURL");
+                String imdbPictureURL = rs.getString("imdbPictureURL");
+                movieList.add(new Movie(id, title, year, imdbPictureURL, 
+                        rtPictureURL, rtAudienceScore));
+            }
+            con.close();
+            stmt.close();
+            rs.close();
+        }
+        catch(SQLException e){
+            e.printStackTrace();
+        }
+        return movieList;
+    }
+    
+    public ArrayList<Movie> getTopMovies(String genre, int limit){
+        String query = "SELECT DISTINCT id, title, year, rtAudienceScore, "
+                + "rtPictureURL, imdbPictureURL FROM movies, movie_genres "
+                + "WHERE movieID=id AND genre LIKE '%" + genre + "%' "
+                + "ORDER BY rtAudienceScore DESC "
+                + "LIMIT " + limit;
         ResultSet rs = null;
         ArrayList<Movie> movieList = new ArrayList<Movie>();
         try{
